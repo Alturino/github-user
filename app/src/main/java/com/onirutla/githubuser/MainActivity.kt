a87eb3655a5d1e3affb5d8aa9f9707aad8b08120
onirutlA <alturino001@gmail.com>
2021-10-21 11:07:37 +0700

    add: behavior when navigate to search fragment

--- app/src/main/java/com/onirutla/githubuser/MainActivity.kt
+++ app/src/main/java/com/onirutla/githubuser/MainActivity.kt
@@ -1,16 +1,77 @@
 package com.onirutla.githubuser
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.onirutla.githubuser.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.findNavController()
+
+        with(binding) {
+            bottomNav.setupWithNavController(navController = navController)
+            fab.setOnClickListener {
+                navController.navigate(MainNavDirections.actionGlobalSearchFragment())
+            }
+        }
+
+        setupDestinationListener(navController)
+    }
+
+    private fun setupDestinationListener(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.searchFragment -> {
+                    setBottomAppBarForSearch()
+                }
+                else -> {
+                    setBottomAppBar()
+                }
+            }
+        }
+    }
+
+    private fun setBottomAppBar() {
+        binding.run {
+            bottomAppbar.visibility = View.VISIBLE
+            bottomAppbar.performShow()
+            fab.show()
+        }
+    }
+
+    private fun setBottomAppBarForSearch() {
+        binding.run {
+            bottomAppbar.visibility = View.GONE
+            bottomAppbar.performHide()
+            fab.hide()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }

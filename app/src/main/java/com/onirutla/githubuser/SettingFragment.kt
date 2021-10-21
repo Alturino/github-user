c51127476f1c900ce75c61b140f5543c791a3d7e
onirutlA <alturino001@gmail.com>
2021-10-21 11:03:20 +0700

    add: about, setting, favorite fragment

---
+++ app/src/main/java/com/onirutla/githubuser/SettingFragment.kt
@@ -0,0 +1,27 @@
+package com.onirutla.githubuser
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.onirutla.githubuser.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}

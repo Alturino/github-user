e8069818ccff8a6b2aa736e731a51e1fe3db6cfd
onirutlA <alturino001@gmail.com>
2021-10-22 21:49:46 +0700

    add: diffUtil in constant

--- app/src/main/java/com/onirutla/githubuser/util/Constant.kt
+++ app/src/main/java/com/onirutla/githubuser/util/Constant.kt
@@ -1,5 +1,16 @@
 package com.onirutla.githubuser.util
 
+import androidx.recyclerview.widget.DiffUtil
+import com.onirutla.githubuser.data.remote.response.UserResponse
+
 object Constant {
     const val BASE_URL = "https://api.github.com/"
+
+    val diff = object : DiffUtil.ItemCallback<UserResponse>() {
+        override fun areItemsTheSame(oldItem: UserResponse, newItem: UserResponse): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: UserResponse, newItem: UserResponse): Boolean =
+            oldItem == newItem
+    }
 }

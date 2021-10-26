e2b2e73a1dde90ea529f91cc27b0d3cc8ea5f4ad
onirutlA <alturino001@gmail.com>
2021-10-26 11:36:18 +0700

    add: add follower fragment

---
+++ app/src/main/java/com/onirutla/githubuser/ui/follower/FollowerFragment.kt
@@ -0,0 +1,28 @@
+package com.onirutla.githubuser.ui.follower
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.onirutla.githubuser.databinding.FragmentFollowerBinding
+
+class FollowerFragment : Fragment() {
+
+    private var _binding: FragmentFollowerBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFollowerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}

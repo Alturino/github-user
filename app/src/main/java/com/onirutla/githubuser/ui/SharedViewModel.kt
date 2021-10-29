ae4f4bd1cfbd244cd5a7bf34738f453edc6d506a
onirutlA <alturino001@gmail.com>
2021-10-29 15:15:07 +0700

    add: shared view model

---
+++ app/src/main/java/com/onirutla/githubuser/ui/SharedViewModel.kt
@@ -0,0 +1,15 @@
+package com.onirutla.githubuser.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SharedViewModel : ViewModel() {
+
+    private val _username = MutableLiveData<String>()
+    val username: LiveData<String> get() = _username
+
+    fun setUsername(username: String) {
+        _username.value = username
+    }
+}

e73593f9116ad6ad412e93eabbf17bf0e20bf74c
onirutlA <alturino001@gmail.com>
2021-11-04 15:41:30 +0700

    add: change call from Remote to call from Repository

--- app/src/main/java/com/onirutla/githubuser/ui/following/FollowingViewModel.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/following/FollowingViewModel.kt
@@ -1,8 +1,9 @@
 package com.onirutla.githubuser.ui.following
 
 import androidx.lifecycle.*
-import com.onirutla.githubuser.data.remote.RemoteDataSource
-import com.onirutla.githubuser.data.remote.response.UserResponse
+import com.onirutla.githubuser.data.Resource
+import com.onirutla.githubuser.data.UserDTO
+import com.onirutla.githubuser.data.source.UserDataSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -10,13 +11,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FollowingViewModel @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
-): ViewModel() {
+    private val userDataSource: UserDataSource
+) : ViewModel() {
 
     private val _username = MutableLiveData<String>()
 
-    val user: LiveData<List<UserResponse>> = _username.switchMap {
-        remoteDataSource.getUserFollowing(it)
+    val user: LiveData<Resource<List<UserDTO>>> = _username.switchMap {
+        userDataSource.getUsersFollowing(it).asLiveData()
     }
 
     fun getUser(username: String) {

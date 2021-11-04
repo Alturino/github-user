4f4cee25a0beef7d85a3087757a8970f39b6e280
onirutlA <alturino001@gmail.com>
2021-11-04 15:34:08 +0700

    add: try-catch to handle exception

--- app/src/main/java/com/onirutla/githubuser/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/onirutla/githubuser/data/source/remote/RemoteDataSource.kt
@@ -1,49 +1,60 @@
 package com.onirutla.githubuser.data.source.remote
 
-import androidx.lifecycle.liveData
 import com.onirutla.githubuser.data.source.remote.network.NetworkService
 import com.onirutla.githubuser.data.source.remote.response.SearchResponse
 import com.onirutla.githubuser.data.source.remote.response.UserResponse
-import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class RemoteDataSource @Inject constructor(private val apiService: NetworkService) {
 
-    suspend fun getUserSearch(username: String): SearchResponse {
-        val response = apiService.getUsersSearch(username)
-        return if (response.isSuccessful) {
-            response.body()!!
-        } else {
-            throw Throwable()
+    suspend fun getUserSearch(username: String): NetworkState<SearchResponse> {
+        return try {
+            val response = apiService.getUsersSearch(username)
+            return if (response.isSuccessful)
+                NetworkState.Success(body = response.body()!!, message = response.message())
+            else
+                NetworkState.Error(message = response.message())
+        } catch (t: Throwable) {
+            NetworkState.Error(message = t.message)
         }
     }
 
-    suspend fun getUserDetail(username: String): UserResponse {
-        val response = apiService.getUserDetail(username = username)
-        return if (response.isSuccessful) {
-            response.body()!!
-        } else {
-            throw Throwable()
+    suspend fun getUserDetail(username: String): NetworkState<UserResponse> {
+        return try {
+            val response = apiService.getUserDetail(username)
+            return if (response.isSuccessful)
+                NetworkState.Success(body = response.body()!!, message = response.message())
+            else
+                NetworkState.Error(message = response.message())
+        } catch (t: Throwable) {
+            NetworkState.Error(message = t.message)
         }
     }
 
-    fun getUserFollower(username: String) = liveData(Dispatchers.IO) {
-        val response = apiService.getUserFollower(username)
-        if (response.isSuccessful) {
-            response.body()?.let {
-                emit(it)
-            }
+    suspend fun getUserFollower(username: String): NetworkState<List<UserResponse>> {
+        return try {
+            val response = apiService.getUserFollower(username)
+            return if (response.isSuccessful)
+                NetworkState.Success(body = response.body()!!, message = response.message())
+            else
+                NetworkState.Error(message = response.message())
+
+        } catch (t: Throwable) {
+            NetworkState.Error(message = t.message)
         }
     }
 
-    fun getUserFollowing(username: String) = liveData(Dispatchers.IO) {
-        val response = apiService.getUserFollowing(username)
-        if (response.isSuccessful) {
-            response.body()?.let {
-                emit(it)
-            }
+    suspend fun getUserFollowing(username: String): NetworkState<List<UserResponse>> {
+        return try {
+            val response = apiService.getUserFollowing(username)
+            return if (response.isSuccessful)
+                NetworkState.Success(body = response.body()!!, message = response.message())
+            else
+                NetworkState.Error(message = response.message())
+        } catch (t: Throwable) {
+            NetworkState.Error(message = t.message)
         }
     }
 

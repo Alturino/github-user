44ef484ba7c0c28e995a2f9c9c65e4c336092995
onirutlA <alturino001@gmail.com>
2021-11-04 15:38:41 +0700

    add: Local Data Source

---
+++ app/src/main/java/com/onirutla/githubuser/data/source/local/LocalDataSource.kt
@@ -0,0 +1,30 @@
+package com.onirutla.githubuser.data.source.local
+
+import com.onirutla.githubuser.data.source.local.dao.UserDao
+import com.onirutla.githubuser.data.source.local.entity.UserEntity
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class LocalDataSource @Inject constructor(private val userDao: UserDao) {
+
+    suspend fun getFavorite(): List<UserEntity> = userDao.getFavorites()
+
+    suspend fun insertUsers(users: List<UserEntity>) = userDao.insertUsers(users)
+
+    suspend fun getUserSearch(username: String) = userDao.getUserSearch(username)
+
+    suspend fun insertUserDetail(userEntity: UserEntity) = userDao.insertUser(userEntity)
+
+    suspend fun getUserDetail(username: String) = userDao.getUserDetail(username)
+
+    suspend fun favorite(userEntity: UserEntity) {
+        userEntity.isFavorite = true
+        userDao.updateFavorite(userEntity)
+    }
+
+    suspend fun unFavorite(userEntity: UserEntity) {
+        userEntity.isFavorite = false
+        userDao.updateFavorite(userEntity)
+    }
+}

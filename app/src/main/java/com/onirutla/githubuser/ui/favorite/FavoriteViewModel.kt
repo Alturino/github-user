f367561d51e182b6f99efe39273e4009e0ff99ef
onirutlA <alturino001@gmail.com>
2021-11-17 22:15:07 +0700

    refactor: change dto and delete dto to entity

--- app/src/main/java/com/onirutla/githubuser/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/favorite/FavoriteViewModel.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.onirutla.githubuser.data.Resource
-import com.onirutla.githubuser.data.UserDTO
 import com.onirutla.githubuser.data.source.UserDataSource
+import com.onirutla.githubuser.data.source.local.entity.UserEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -15,7 +15,7 @@ class FavoriteViewModel @Inject constructor(
     private val userDataSource: UserDataSource
 ) : ViewModel() {
 
-    val favorites: LiveData<Resource<List<UserDTO>>> =
+    val favorites: LiveData<Resource<List<UserEntity>>> =
         userDataSource.getUsersFavorite().asLiveData(viewModelScope.coroutineContext)
 
 

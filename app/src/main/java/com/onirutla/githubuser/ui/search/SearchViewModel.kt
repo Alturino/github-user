10a06538490d24168aeb6932dc22f258890bf45d
onirutlA <alturino001@gmail.com>
2021-11-20 12:48:48 +0700

    refactor: change live data to sharedflow and stateflow on search view model

--- app/src/main/java/com/onirutla/githubuser/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/search/SearchViewModel.kt
@@ -1,11 +1,13 @@
 package com.onirutla.githubuser.ui.search
 
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.onirutla.githubuser.data.Resource
 import com.onirutla.githubuser.data.source.UserDataSource
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,15 +17,19 @@ class SearchViewModel @Inject constructor(
     private val userDataSource: UserDataSource
 ) : ViewModel() {
 
-    private val _username = MutableLiveData<String>()
+    private val _username = MutableSharedFlow<String>()
 
-    val users: LiveData<Resource<List<UserEntity>>> = _username.switchMap {
-        userDataSource.getUsersSearch(it).asLiveData()
-    }
+    val users: StateFlow<Resource<List<UserEntity>>> = _username.flatMapLatest {
+        userDataSource.getUsersSearch(it)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = Resource.Loading()
+    )
 
     fun search(username: String) {
         viewModelScope.launch {
-            _username.value = username
+            _username.emit(username)
         }
     }
 

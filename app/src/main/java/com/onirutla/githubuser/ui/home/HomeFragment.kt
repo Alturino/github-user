5951df0c47d8c32df898e42efe911342f0a5b7cf
onirutlA <alturino001@gmail.com>
2021-11-28 12:47:14 +0700

    add: fade through animation to setting, about, favorite, home fragment

--- app/src/main/java/com/onirutla/githubuser/ui/home/HomeFragment.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/home/HomeFragment.kt
@@ -1,16 +1,30 @@
 package com.onirutla.githubuser.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.material.transition.MaterialFadeThrough
+import com.onirutla.githubuser.R
 import com.onirutla.githubuser.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enterTransition = MaterialFadeThrough().apply {
+            duration = resources.getInteger(R.integer.material_motion_duration_long_1).toLong()
+        }
+
+        exitTransition = MaterialFadeThrough().apply {
+            duration = resources.getInteger(R.integer.material_motion_duration_long_1).toLong()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,

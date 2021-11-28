aa974c0f0cc760502273609d59cb0c57b694c54b
onirutlA <alturino001@gmail.com>
2021-11-28 10:50:16 +0700

    add: change return type to dto

--- app/src/main/java/com/onirutla/githubuser/data/source/UserDataSource.kt
+++ app/src/main/java/com/onirutla/githubuser/data/source/UserDataSource.kt
@@ -1,19 +1,19 @@
 package com.onirutla.githubuser.data.source
 
 import com.onirutla.githubuser.data.Resource
-import com.onirutla.githubuser.data.source.local.entity.UserEntity
+import com.onirutla.githubuser.data.UserDTO
 import kotlinx.coroutines.flow.Flow
 
 interface UserDataSource {
-    fun getUsersSearch(username: String): Flow<Resource<List<UserEntity>>>
+    fun getUsersSearch(username: String): Flow<Resource<List<UserDTO>>>
 
-    fun getUserDetail(username: String): Flow<Resource<UserEntity>>
+    fun getUserDetail(username: String): Flow<Resource<UserDTO>>
 
-    fun getUsersFollower(username: String): Flow<Resource<List<UserEntity>>>
+    fun getUsersFollower(username: String): Flow<Resource<List<UserDTO>>>
 
-    fun getUsersFollowing(username: String): Flow<Resource<List<UserEntity>>>
+    fun getUsersFollowing(username: String): Flow<Resource<List<UserDTO>>>
 
-    fun getUsersFavorite(): Flow<Resource<List<UserEntity>>>
+    fun getUsersFavorite(): Flow<Resource<List<UserDTO>>>
 
-    suspend fun setUserFavorite(userEntity: UserEntity)
+    suspend fun setUserFavorite(userDto: UserDTO)
 }

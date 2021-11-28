368904876f42ac82101df387a097772f617c091d
onirutlA <alturino001@gmail.com>
2021-11-28 10:51:20 +0700

    add: change type to dto

--- app/src/main/java/com/onirutla/githubuser/ui/following/FollowingViewModel.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/following/FollowingViewModel.kt
@@ -2,6 +2,7 @@ package com.onirutla.githubuser.ui.following
 
 import androidx.lifecycle.*
 import com.onirutla.githubuser.data.Resource
+import com.onirutla.githubuser.data.UserDTO
 import com.onirutla.githubuser.data.source.UserDataSource
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,7 +17,7 @@ class FollowingViewModel @Inject constructor(
 
     private val _username = MutableLiveData<String>()
 
-    val user: LiveData<Resource<List<UserEntity>>> = _username.switchMap {
+    val user: LiveData<Resource<List<UserDTO>>> = _username.switchMap {
         userDataSource.getUsersFollowing(it).asLiveData()
     }
 

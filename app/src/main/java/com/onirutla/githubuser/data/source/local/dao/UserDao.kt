45cbe9938af0e9482843e9f2febe0ae4de8a7970
onirutlA <alturino001@gmail.com>
2021-12-03 19:20:12 +0700

    refactor: getUserDetail, getUserSearch to be reactive

--- app/src/main/java/com/onirutla/githubuser/data/source/local/dao/UserDao.kt
+++ app/src/main/java/com/onirutla/githubuser/data/source/local/dao/UserDao.kt
@@ -20,13 +20,13 @@ interface UserDao {
     @Update
     suspend fun updateFavorite(userEntity: UserEntity)
 
-    @Query(value = "SELECT * FROM USER where username like '%' || :username || '%'")
-    suspend fun getUserDetail(username: String): UserEntity
+    @Query(value = "SELECT * FROM USER where username = :username")
+    fun getUserDetail(username: String): Flow<UserEntity?>
 
     @Query(value = "SELECT * FROM USER where is_favorite = 1")
     fun getFavorites(): Flow<List<UserEntity>>
 
     @Query(value = "SELECT * FROM USER WHERE username like '%' || :username || '%'")
-    suspend fun getUserSearch(username: String): List<UserEntity>
+    fun getUserSearch(username: String): Flow<List<UserEntity>>
 
 }

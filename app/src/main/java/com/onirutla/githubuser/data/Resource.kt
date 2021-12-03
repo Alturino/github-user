b4da8b88db51ca7fab0e03f64bee23cf42bd1657
onirutlA <alturino001@gmail.com>
2021-12-03 19:22:57 +0700

    refactor: change loading to data class

--- app/src/main/java/com/onirutla/githubuser/data/Resource.kt
+++ app/src/main/java/com/onirutla/githubuser/data/Resource.kt
@@ -3,5 +3,5 @@ package com.onirutla.githubuser.data
 sealed class Resource<T> {
     data class Success<T>(val data: T) : Resource<T>()
     data class Error<T>(val message: String?) : Resource<T>()
-    class Loading<T> : Resource<T>()
+    data class Loading<T>(val data: T? = null) : Resource<T>()
 }

7c3f58d1e2cbc3c29a72e5705025407f3a5b74f6
onirutlA <alturino001@gmail.com>
2021-12-11 22:18:15 +0700

    add: SplashScreenViewModel

---
+++ app/src/main/java/com/onirutla/githubuser/ui/splashscreen/SplashScreenViewModel.kt
@@ -0,0 +1,18 @@
+package com.onirutla.githubuser.ui.splashscreen
+
+import android.annotation.SuppressLint
+import androidx.lifecycle.ViewModel
+import com.onirutla.githubuser.util.DataStoreManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@SuppressLint("CustomSplashScreen")
+@HiltViewModel
+class SplashScreenViewModel @Inject constructor(
+    dataStoreManager: DataStoreManager
+) : ViewModel() {
+
+    val userPreferences: Flow<Boolean> = dataStoreManager.preferencesIsSeen
+
+}

7230f69b8292010c9fe9a0dc030a34257edc7e33
onirutlA <alturino001@gmail.com>
2021-12-20 19:20:08 +0700

    refactor: add localdatasource interface and it's implementation

--- app/src/main/java/com/onirutla/githubuser/data/source/local/LocalDataSource.kt
+++ app/src/main/java/com/onirutla/githubuser/data/source/local/LocalDataSource.kt
@@ -1,53 +1,22 @@
 package com.onirutla.githubuser.data.source.local
 
-import com.onirutla.githubuser.data.source.local.dao.UserDao
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
-class LocalDataSource @Inject constructor(private val userDao: UserDao) {
+interface LocalDataSource {
 
-    fun getUserSearch(username: String): Flow<FromDb<List<UserEntity>>> =
-        userDao.getUserSearch(username).map {
-            if (it.isNullOrEmpty())
-                FromDb.Empty("You don't have any favorite yet")
-            else
-                FromDb.Success(it)
-        }
+    fun getUserSearch(username: String): Flow<FromDb<List<UserEntity>>>
 
-    fun getFavorite(): Flow<FromDb<List<UserEntity>>> = userDao.getFavorites().map {
-        if (it.isNullOrEmpty())
-            FromDb.Empty("You don't have any favorite yet")
-        else
-            FromDb.Success(it)
-    }
+    fun getFavorite(): Flow<FromDb<List<UserEntity>>>
 
+    fun getUserDetail(username: String): Flow<FromDb<UserEntity>>
 
-    fun getUserDetail(username: String): Flow<FromDb<UserEntity>> =
-        userDao.getUserDetail(username).map {
-            return@map if (it == null) {
-                FromDb.Empty("User not found in database")
-            } else {
-                FromDb.Success(it)
-            }
-        }
+    suspend fun insertUsers(users: List<UserEntity>)
 
-    suspend fun insertUsers(users: List<UserEntity>) = userDao.insertUsers(users)
+    suspend fun insertUserDetail(userEntity: UserEntity)
 
-    suspend fun insertUserDetail(userEntity: UserEntity) = userDao.insertUser(userEntity)
+    suspend fun favorite(userEntity: UserEntity): UserEntity
 
-    suspend fun favorite(userEntity: UserEntity): UserEntity {
-        val favorite = userEntity.copy(isFavorite = true)
-        userDao.updateFavorite(favorite)
-        return favorite
-    }
+    suspend fun unFavorite(userEntity: UserEntity): UserEntity
 
-    suspend fun unFavorite(userEntity: UserEntity): UserEntity {
-        val unFavorite = userEntity.copy(isFavorite = false)
-        userDao.updateFavorite(unFavorite)
-        return unFavorite
-    }
 }

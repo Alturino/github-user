586f872b1583d164b4e049f6381d0e9c8569b5fe
onirutlA <alturino001@gmail.com>
2022-02-24 13:38:06 +0700

    add: navHostFragment and navController to main Activity

--- app/src/main/java/com/onirutla/githubuser/MainActivity.kt
+++ app/src/main/java/com/onirutla/githubuser/MainActivity.kt
@@ -2,15 +2,23 @@ package com.onirutla.githubuser
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.onirutla.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
     }
 }

52e756532543ed418dc31bde9110fb9ec5dcd676
onirutlA <alturino001@gmail.com>
2022-02-24 16:27:45 +0700

    add: back icon navigation click listener

--- app/src/main/java/com/onirutla/githubuser/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/detail/DetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.onirutla.githubuser.databinding.FragmentDetailBinding
 
@@ -25,7 +26,10 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.user = args.user
+        binding.apply {
+            user = args.user
+            toolbar.setNavigationOnClickListener { it.findNavController().navigateUp() }
+        }
     }
 
     override fun onDestroyView() {

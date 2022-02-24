e5fc6122881fd34bab4ad2c5631ab2bd20bacbbf
onirutlA <alturino001@gmail.com>
2022-02-24 13:33:08 +0700

    add: args to databinding in detail fragment

--- app/src/main/java/com/onirutla/githubuser/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/detail/DetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.onirutla.githubuser.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
@@ -12,12 +13,19 @@ class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
 
+    private val args: DetailFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.user = args.user
     }
 
     override fun onDestroyView() {

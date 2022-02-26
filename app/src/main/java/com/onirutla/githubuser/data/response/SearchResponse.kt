96aea0613d8250c73ab3fb410eb8839f936bd44d
onirutlA <alturino001@gmail.com>
2022-02-26 21:16:47 +0700

    add: data holder for search response and user response

---
+++ app/src/main/java/com/onirutla/githubuser/data/response/SearchResponse.kt
@@ -0,0 +1,18 @@
+package com.onirutla.githubuser.data.response
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SearchResponse(
+
+    @Json(name = "incomplete_results")
+    val incompleteResults: Boolean? = null,
+
+    @Json(name = "items")
+    val items: List<UserResponse>? = null,
+
+    @Json(name = "total_count")
+    val totalCount: Int? = null
+
+)
---
+++ app/src/main/java/com/onirutla/githubuser/data/response/UserResponse.kt
@@ -0,0 +1,57 @@
+package com.onirutla.githubuser.data.response
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class UserResponse(
+
+    @Json(name = "following_url")
+    val followingUrl: String? = null,
+
+    @Json(name = "bio")
+    val bio: String? = null,
+
+    @Json(name = "created_at")
+    val createdAt: String? = null,
+
+    @Json(name = "login")
+    val username: String? = null,
+
+    @Json(name = "type")
+    val type: String? = null,
+
+    @Json(name = "blog")
+    val blog: String? = null,
+
+    @Json(name = "company")
+    val company: String? = null,
+
+    @Json(name = "id")
+    val id: Int? = null,
+
+    @Json(name = "public_repos")
+    val publicRepos: Int? = null,
+
+    @Json(name = "followers_url")
+    val followersUrl: String? = null,
+
+    @Json(name = "public_gists")
+    val publicGists: Int? = null,
+
+    @Json(name = "followers")
+    val followers: Int? = null,
+
+    @Json(name = "avatar_url")
+    val avatarUrl: String? = null,
+
+    @Json(name = "following")
+    val following: Int? = null,
+
+    @Json(name = "name")
+    val name: String? = null,
+
+    @Json(name = "location")
+    val location: String? = null,
+
+)

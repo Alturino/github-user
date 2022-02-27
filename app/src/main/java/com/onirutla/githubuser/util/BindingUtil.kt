707a586787f6cde1e10e0155d95a05dd3f51c727
onirutlA <alturino001@gmail.com>
2022-02-27 09:12:12 +0700

    refactor: load image in binding util

--- app/src/main/java/com/onirutla/githubuser/util/BindingUtil.kt
+++ app/src/main/java/com/onirutla/githubuser/util/BindingUtil.kt
@@ -4,11 +4,6 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("loadImage")
-fun loadImage(view: ImageView, fileName: String) {
-    fileName.let {
-        val context = view.context
-        GlideApp.with(view)
-            .load(context.resources.getIdentifier(it, "drawable", context.packageName))
-            .into(view)
-    }
+fun loadImage(view: ImageView, any: Any?) {
+    any?.let { GlideApp.with(view).load(it).into(view) }
 }

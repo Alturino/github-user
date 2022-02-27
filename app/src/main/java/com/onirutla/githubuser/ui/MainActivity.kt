99684bf4accf00240f1a24b69473807627a0448b
onirutlA <alturino001@gmail.com>
2022-02-27 09:19:00 +0700

    add: annotation AndroidEntryPoint at mainactivity

--- app/src/main/java/com/onirutla/githubuser/ui/MainActivity.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.onirutla.githubuser.R
 import com.onirutla.githubuser.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding

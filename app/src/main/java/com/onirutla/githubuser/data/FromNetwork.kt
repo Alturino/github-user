ccf7a0cb8f08386b2b91b39bd866842a2680b254
onirutlA <alturino001@gmail.com>
2022-02-27 09:17:10 +0700

    refactor: delete loading in FromNetwork.kt

--- app/src/main/java/com/onirutla/githubuser/data/FromNetwork.kt
+++ app/src/main/java/com/onirutla/githubuser/data/FromNetwork.kt
@@ -3,4 +3,5 @@ package com.onirutla.githubuser.data
 sealed class FromNetwork<T> {
     data class Success<T>(val body: T, val message: String? = null) : FromNetwork<T>()
     data class Error<T>(val data: T? = null, val message: String? = null) : FromNetwork<T>()
+    data class Loading<T>(val data: T? = null): FromNetwork<T>()
 }

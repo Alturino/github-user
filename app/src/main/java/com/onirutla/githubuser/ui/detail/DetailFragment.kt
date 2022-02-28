7c628a9bdcbbeebfb497aa43d4685bb68ef5780e
onirutlA <alturino001@gmail.com>
2022-02-28 22:36:06 +0700

    add: detailviewmodel and DetailFragment.kt observe to DetailViewModel.kt

--- app/src/main/java/com/onirutla/githubuser/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/detail/DetailFragment.kt
@@ -1,20 +1,33 @@
 package com.onirutla.githubuser.ui.detail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.tabs.TabLayoutMediator
+import com.onirutla.githubuser.R
+import com.onirutla.githubuser.data.Resource
 import com.onirutla.githubuser.databinding.FragmentDetailBinding
+import com.onirutla.githubuser.ui.SharedViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
 
     private val args: DetailFragmentArgs by navArgs()
+
+    private val viewModel: DetailViewModel by viewModels()
+    private val sharedViewModel: SharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +39,49 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        args.username?.let {
+            viewModel.getUserDetail(it)
+            sharedViewModel.setUsername(it)
+        }
+
+        viewModel.user.observe(viewLifecycleOwner, {
+            when (it) {
+                is Resource.Error -> {
+                    binding.apply {
+                        progressBar.hide()
+                        progressBar.visibility = View.GONE
+                    }
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                }
+                is Resource.Loading -> {
+                    binding.apply {
+                        progressBar.visibility = View.VISIBLE
+                        progressBar.show()
+                    }
+                }
+                is Resource.Success -> {
+                    binding.apply {
+                        Log.d("detail success", "${it.data}")
+                        user = it.data
+                        progressBar.hide()
+                        progressBar.visibility = View.GONE
+                    }
+                }
+            }
+        })
+
+        val tabAdapter = TabAdapter(requireActivity())
+
         binding.apply {
-            user = args.user
             toolbar.setNavigationOnClickListener { it.findNavController().navigateUp() }
+            viewPager.adapter = tabAdapter
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                val titles = listOf(
+                    resources.getString(R.string.follower),
+                    resources.getString(R.string.following)
+                )
+                tab.text = titles[position]
+            }.attach()
         }
     }
 
---
+++ app/src/main/java/com/onirutla/githubuser/ui/detail/DetailViewModel.kt
@@ -0,0 +1,35 @@
+package com.onirutla.githubuser.ui.detail
+
+import androidx.lifecycle.*
+import com.onirutla.githubuser.data.FromNetwork
+import com.onirutla.githubuser.data.Resource
+import com.onirutla.githubuser.data.repository.Repository
+import com.onirutla.githubuser.data.response.UserResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    val username = MutableLiveData<String>()
+
+    val user: LiveData<Resource<UserResponse>> = username.switchMap { username ->
+        liveData {
+            repository.getUserDetail(username).collect { fromNetwork ->
+                when (fromNetwork) {
+                    is FromNetwork.Error -> emit(Resource.Error(fromNetwork.message))
+                    is FromNetwork.Loading -> emit(Resource.Loading())
+                    is FromNetwork.Success -> emit(Resource.Success(fromNetwork.data))
+                }
+            }
+        }
+    }
+
+    fun getUserDetail(username: String) {
+        this.username.value = username
+    }
+
+}

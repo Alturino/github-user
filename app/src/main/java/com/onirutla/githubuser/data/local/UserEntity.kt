6a9bc872b6938d51963fcad6387b42bfb2ec840b
onirutlA <alturino001@gmail.com>
2022-03-02 21:31:31 +0700

    add: user entity

---
+++ app/src/main/java/com/onirutla/githubuser/data/local/UserEntity.kt
@@ -0,0 +1,42 @@
+package com.onirutla.githubuser.data.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+data class UserEntity(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo
+    val id: Int,
+
+    @ColumnInfo
+    val username: String,
+
+    @ColumnInfo
+    val name: String,
+
+    @ColumnInfo
+    val type: String,
+
+    @ColumnInfo
+    val followers: Int,
+
+    @ColumnInfo
+    val following: Int,
+
+    @ColumnInfo(name = "public_repos")
+    val publicRepos: Int,
+
+    @ColumnInfo(name = "follower_url")
+    val followersUrl: String,
+
+    @ColumnInfo(name = "following_url")
+    val followingUrl: String,
+
+    @ColumnInfo(name = "avatar_url")
+    val avatarUrl: String,
+
+    @ColumnInfo(name = "is_favorite")
+    val isFavorite: Boolean = false
+)

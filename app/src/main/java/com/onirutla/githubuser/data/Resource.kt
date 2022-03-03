811e8aaa69e2f9708ec5862f212a2c0d82343d00
onirutlA <alturino001@gmail.com>
2022-03-03 23:52:49 +0700

    refactor: rename resource.error to resource.empty

--- app/src/main/java/com/onirutla/githubuser/data/Resource.kt
+++ app/src/main/java/com/onirutla/githubuser/data/Resource.kt
@@ -2,6 +2,6 @@ package com.onirutla.githubuser.data
 
 sealed class Resource<T> {
     data class Success<T>(val data: T) : Resource<T>()
-    data class Error<T>(val message: String? = null) : Resource<T>()
+    data class Empty<T>(val message: String? = null) : Resource<T>()
     data class Loading<T>(val data: T? = null) : Resource<T>()
 }

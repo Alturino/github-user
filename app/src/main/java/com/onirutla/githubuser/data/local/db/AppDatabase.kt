5a07edd05f609640fd869b6acaa11b78a47ba141
onirutlA <alturino001@gmail.com>
2022-03-03 23:47:55 +0700

    refactor: move AppDatabase.kt from local to local.db

--- app/src/main/java/com/onirutla/githubuser/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/onirutla/githubuser/data/local/db/AppDatabase.kt
@@ -1,7 +1,9 @@
-package com.onirutla.githubuser.data.local
+package com.onirutla.githubuser.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.onirutla.githubuser.data.local.dao.UserDao
+import com.onirutla.githubuser.data.local.entity.UserEntity
 
 @Database(entities = [UserEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {

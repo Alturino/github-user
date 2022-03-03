866579bbbd6e0fd0f27e716be60e672966ad47b6
onirutlA <alturino001@gmail.com>
2022-03-03 23:54:50 +0700

    refactor: delete un needed nested object

--- app/src/main/java/com/onirutla/githubuser/di/DatabaseModule.kt
+++ app/src/main/java/com/onirutla/githubuser/di/DatabaseModule.kt
@@ -2,8 +2,8 @@ package com.onirutla.githubuser.di
 
 import android.content.Context
 import androidx.room.Room
-import com.onirutla.githubuser.data.local.AppDatabase
-import com.onirutla.githubuser.data.local.UserDao
+import com.onirutla.githubuser.data.local.dao.UserDao
+import com.onirutla.githubuser.data.local.db.AppDatabase
 import com.onirutla.githubuser.util.Constant
 import dagger.Module
 import dagger.Provides
@@ -12,22 +12,20 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
-    @Module
-    @InstallIn(SingletonComponent::class)
-    object DatabaseModule {
-        @Singleton
-        @Provides
-        fun providesDatabase(@ApplicationContext context: Context): AppDatabase =
-            Room.databaseBuilder(
-                context,
-                AppDatabase::class.java,
-                Constant.DB_NAME
-            ).fallbackToDestructiveMigration()
+    @Singleton
+    @Provides
+    fun providesDatabase(@ApplicationContext context: Context): AppDatabase =
+        Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            Constant.DB_NAME
+        ).fallbackToDestructiveMigration()
                 .build()
 
         @Singleton
         @Provides
         fun providesUserDao(db: AppDatabase): UserDao = db.userDao
     }
-}

800b55f212c004de340fb4157c4cfb2cca5bb380
onirutlA <alturino001@gmail.com>
2022-03-03 23:57:16 +0700

    refactor: move from local to local.dao

--- app/src/main/java/com/onirutla/githubuser/data/local/dao/UserDao.kt
+++ app/src/main/java/com/onirutla/githubuser/data/local/dao/UserDao.kt
@@ -1,10 +1,12 @@
-package com.onirutla.githubuser.data.local
+package com.onirutla.githubuser.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy.ABORT
 import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import androidx.room.Update
+import com.onirutla.githubuser.data.local.entity.UserEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -13,19 +15,19 @@ interface UserDao {
     @Insert(onConflict = REPLACE)
     suspend fun insertUser(userEntity: UserEntity)
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = ABORT)
     suspend fun insertUsers(users: List<UserEntity>)
 
     @Update
-    suspend fun updateFavorite(userEntity: UserEntity)
+    suspend fun updateFavorite(user: UserEntity)
 
-    @Query(value = "SELECT * FROM USER where username = :username")
+    @Query(value = "SELECT * FROM USER WHERE username = :username")
     fun getUserDetail(username: String): Flow<UserEntity?>
 
-    @Query(value = "SELECT * FROM USER where is_favorite = 1")
+    @Query(value = "SELECT * FROM USER WHERE is_favorite = 1")
     fun getFavorites(): Flow<List<UserEntity>>
 
     @Query(value = "SELECT * FROM USER WHERE username like :username || '%'")
-    fun getUserSearch(username: String): Flow<List<UserEntity>>
+    fun findUserByUsername(username: String): Flow<List<UserEntity>>
 
 }

be5bbb0a0477827da456e63a3f6b35a89de86441
onirutlA <alturino001@gmail.com>
2022-03-03 23:54:06 +0700

    refactor: remove remote and local state wrapper

--- app/src/main/java/com/onirutla/githubuser/data/local/FromDb.kt
+++
@@ -1,6 +0,0 @@
-package com.onirutla.githubuser.data.local
-
-sealed class FromDb<T> {
-    data class Success<T>(val data: T) : FromDb<T>()
-    data class Empty<T>(val message: String? = null) : FromDb<T>()
-}

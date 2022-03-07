f667f7db6c16adfc84d8eeabbc420cb5c647beed
onirutlA <alturino001@gmail.com>
2022-03-07 07:41:15 +0700

    add: Favorite Fragment and Favorite View Model

---
+++ app/src/main/java/com/onirutla/githubuser/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,71 @@
+package com.onirutla.githubuser.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import com.onirutla.githubuser.data.Resource
+import com.onirutla.githubuser.databinding.FragmentFavoriteBinding
+import com.onirutla.githubuser.ui.adapter.UserAdapter
+
+class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: FavoriteViewModel by viewModels()
+    private val userAdapter by lazy {
+        UserAdapter { user, view ->
+            view.findNavController()
+                .navigate(
+                    FavoriteFragmentDirections.actionFavoriteFragmentToDetailFragment(user.username)
+                )
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.favoriteUser.observe(viewLifecycleOwner, {
+            when (it) {
+                is Resource.Empty -> {
+                    binding.apply {
+                        progressBar.hide()
+                        progressBar.visibility = View.GONE
+                    }
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                }
+                is Resource.Loading -> {
+                    binding.apply {
+                        progressBar.visibility = View.VISIBLE
+                        progressBar.show()
+                    }
+                }
+                is Resource.Success -> {
+                    binding.apply {
+                        progressBar.hide()
+                        progressBar.visibility = View.GONE
+                    }
+                    userAdapter.submitList(it.data)
+                }
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/onirutla/githubuser/ui/favorite/FavoriteViewModel.kt
@@ -0,0 +1,16 @@
+package com.onirutla.githubuser.ui.favorite
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.onirutla.githubuser.data.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoriteViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    val favoriteUser = userRepository.getFavoriteUsers().asLiveData()
+
+}

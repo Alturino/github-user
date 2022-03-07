c4d99566672e77513a6180faf4adda25649de23f
onirutlA <alturino001@gmail.com>
2022-03-08 01:11:44 +0700

    add: viewModelScope in FavoriteViewModel.kt

--- app/src/main/java/com/onirutla/githubuser/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/favorite/FavoriteViewModel.kt
@@ -2,6 +2,7 @@ package com.onirutla.githubuser.ui.favorite
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.onirutla.githubuser.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -11,6 +12,6 @@ class FavoriteViewModel @Inject constructor(
     private val userRepository: UserRepository
 ) : ViewModel() {
 
-    val favoriteUser = userRepository.getFavoriteUsers().asLiveData()
+    val favoriteUser = userRepository.getFavoriteUsers().asLiveData(viewModelScope.coroutineContext)
 
 }

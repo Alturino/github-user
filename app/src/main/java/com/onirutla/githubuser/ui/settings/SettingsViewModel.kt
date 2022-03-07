9f961a99e016d629c21790ad12fc4c5a9b7b4afc
onirutlA <alturino001@gmail.com>
2022-03-07 13:34:02 +0700

    refactor: remove root_preferences to add datastore preference

---
+++ app/src/main/java/com/onirutla/githubuser/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,24 @@
+package com.onirutla.githubuser.ui.settings
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.onirutla.githubuser.data.preference.UserDataStore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val userDataStore: UserDataStore
+) : ViewModel() {
+
+    val isDarkTheme = userDataStore.isDarkTheme.asLiveData()
+
+    fun setIsDarkTheme(isDarkTheme: Boolean) {
+        viewModelScope.launch {
+            userDataStore.setIsDarkTheme(isDarkTheme)
+        }
+    }
+
+}

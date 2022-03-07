106f1781b7ceaa6eccee7b45f5e9e687b96d0b20
onirutlA <alturino001@gmail.com>
2022-03-07 12:12:59 +0700

    add: behavior for bottom nav

--- app/src/main/java/com/onirutla/githubuser/ui/MainActivity.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.onirutla.githubuser.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.onirutla.githubuser.R
 import com.onirutla.githubuser.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,7 +25,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
+
+        binding.apply {
+            bottomNav.setupWithNavController(navController = navController)
+        }
+
+        setupDestinationListener(navController)
     }
+
+    private fun setupDestinationListener(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.searchFragment -> hideBottomAppbar()
+                R.id.detailFragment -> hideBottomAppbar()
+                else -> showBottomAppbar()
+            }
+        }
+    }
+
+    private fun showBottomAppbar() {
+        binding.run {
+            bottomAppbar.visibility = View.VISIBLE
+            bottomAppbar.performShow()
+        }
+    }
+
+    private fun hideBottomAppbar() {
+        binding.run {
+            bottomAppbar.visibility = View.GONE
+            bottomAppbar.performHide()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
 }

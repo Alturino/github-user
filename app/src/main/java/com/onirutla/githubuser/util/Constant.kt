6954db0d6f96f59d4d217877f9c2c5a073b8c46e
onirutlA <alturino001@gmail.com>
2022-03-07 11:56:06 +0700

    refactor: rename variable on constant

--- app/src/main/java/com/onirutla/githubuser/util/Constant.kt
+++ app/src/main/java/com/onirutla/githubuser/util/Constant.kt
@@ -4,7 +4,7 @@ import com.onirutla.githubuser.BuildConfig
 
 const val DELAY_TO_MAIN_ACTIVITY = 2000L
 const val DB_NAME = BuildConfig.DB_NAME
-const val KEY_IS_DARK_THEME = BuildConfig.KEY_USER_PREFERENCES
+const val KEY_IS_DARK_THEME = BuildConfig.KEY_IS_DARK_THEME
 const val USER_DATASTORE = BuildConfig.USER_DATASTORE
 const val BASE_URL = BuildConfig.BASE_URL
 const val API_KEY = BuildConfig.API_KEY

06ba352b644b0bfd4f89301ea4e55d9ceed16ed3
onirutlA <alturino001@gmail.com>
2022-04-25 14:36:21 +0700

    refactor: remove constant diff util to adapter file

--- app/src/main/java/com/onirutla/githubuser/util/Constant.kt
+++ app/src/main/java/com/onirutla/githubuser/util/Constant.kt
@@ -1,18 +1,7 @@
 package com.onirutla.githubuser.util
 
-import androidx.recyclerview.widget.DiffUtil
-import com.onirutla.githubuser.data.UserDTO
-
 object Constant {
     const val BASE_URL = "https://api.github.com/"
-
-    val diff = object : DiffUtil.ItemCallback<UserDTO>() {
-        override fun areItemsTheSame(oldItem: UserDTO, newItem: UserDTO): Boolean =
-            oldItem.id == newItem.id
-
-        override fun areContentsTheSame(oldItem: UserDTO, newItem: UserDTO): Boolean =
-            oldItem == newItem
-    }
     const val DB_NAME = "github_user_db"
 
     const val INTRO_DATASTORE = "intro_datastore"

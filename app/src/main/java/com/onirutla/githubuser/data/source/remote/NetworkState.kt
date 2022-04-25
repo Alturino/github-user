1745297e6535fe987adf8355aaf035666257fcf2
onirutlA <alturino001@gmail.com>
2022-04-25 17:14:05 +0700

    refactor: rename from NetworkState.kt to Response.kt

--- app/src/main/java/com/onirutla/githubuser/data/source/remote/NetworkState.kt
+++
@@ -1,6 +0,0 @@
-package com.onirutla.githubuser.data.source.remote
-
-sealed class NetworkState<T> {
-    data class Success<T>(val body: T, val message: String? = null) : NetworkState<T>()
-    data class Error<T>(val data: T? = null, val message: String? = null) : NetworkState<T>()
-}

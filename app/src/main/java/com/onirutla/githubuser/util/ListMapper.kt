961dc4231a1d8643fe3d4fbdf26609ef4c4f30ab
onirutlA <alturino001@gmail.com>
2022-04-25 15:56:18 +0700

    delete listmapper

--- app/src/main/java/com/onirutla/githubuser/util/ListMapper.kt
+++
@@ -1,9 +0,0 @@
-package com.onirutla.githubuser.util
-
-inline fun <I, O> mapList(input: List<I>, mapListItem: (I) -> O): List<O> {
-    return input.map { mapListItem(it) }
-}
-
-inline fun <I, O> mapNullInputList(input: List<I>?, mapListItem: (I) -> O): List<O> {
-    return input?.map { mapListItem(it) } ?: emptyList()
-}

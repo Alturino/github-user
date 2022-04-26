db5a101f8dcd453b10f516928dbe2d9e2fbf6228
onirutlA <alturino001@gmail.com>
2022-04-26 20:12:11 +0700

    refactor: UserAdapter.kt to use Comparator

--- app/src/main/java/com/onirutla/githubuser/ui/adapter/UserAdapter.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/adapter/UserAdapter.kt
@@ -3,22 +3,15 @@ package com.onirutla.githubuser.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
 import com.onirutla.githubuser.databinding.UserItemBinding
+import com.onirutla.githubuser.util.Comparator
 
-class UserAdapter(private val listener: (view: View, user: UserEntity) -> Unit) :
-    ListAdapter<UserEntity, UserAdapter.ViewHolder>(Diff) {
-
-    object Diff : DiffUtil.ItemCallback<UserEntity>() {
-        override fun areItemsTheSame(oldItem: UserEntity, newItem: UserEntity): Boolean =
-            oldItem.id == newItem.id
-
-        override fun areContentsTheSame(oldItem: UserEntity, newItem: UserEntity): Boolean =
-            oldItem == newItem
-    }
+class UserAdapter(
+    private val listener: (view: View, user: UserEntity) -> Unit
+) : ListAdapter<UserEntity, UserAdapter.ViewHolder>(Comparator) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = UserItemBinding.inflate(

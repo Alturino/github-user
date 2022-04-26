0c6b6e71bcfbf2a1d4c78c4faa8834bc30fc3e11
onirutlA <alturino001@gmail.com>
2022-04-26 20:15:26 +0700

    refactor: change from list to pagingdata

--- app/src/main/java/com/onirutla/githubuser/ui/follower/FollowerViewModel.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/follower/FollowerViewModel.kt
@@ -2,14 +2,19 @@ package com.onirutla.githubuser.ui.follower
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.onirutla.githubuser.data.Resource
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.onirutla.githubuser.data.repository.UserRepository
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
 import com.onirutla.githubuser.util.MainDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,13 +27,14 @@ class FollowerViewModel @Inject constructor(
 
     private val _username = MutableSharedFlow<String>()
 
-    val user: StateFlow<Resource<List<UserEntity>>> = _username.flatMapLatest {
-        userRepository.getFollowerBy(it)
-    }.stateIn(
-        viewModelScope,
-        started = SharingStarted.WhileSubscribed(5000),
-        initialValue = Resource.Loading()
-    )
+    val user: StateFlow<PagingData<UserEntity>> = _username.flatMapLatest {
+        userRepository.getFollowingPaging(it)
+    }.cachedIn(viewModelScope)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = PagingData.empty()
+        )
 
     fun getUser(username: String) {
         viewModelScope.launch(dispatcher) {

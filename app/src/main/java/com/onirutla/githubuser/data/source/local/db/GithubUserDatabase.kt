bf2f89a6c1e3fa6da649de75da97ca8878c205db
onirutlA <alturino001@gmail.com>
2022-04-29 14:24:58 +0700

    add: UserRemoteKey.kt to database add: UserRemoteKeyDao.kt to database

--- app/src/main/java/com/onirutla/githubuser/data/source/local/db/GithubUserDatabase.kt
+++ app/src/main/java/com/onirutla/githubuser/data/source/local/db/GithubUserDatabase.kt
@@ -2,10 +2,13 @@ package com.onirutla.githubuser.data.source.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.onirutla.githubuser.data.source.local.dao.UserRemoteKeyDao
 import com.onirutla.githubuser.data.source.local.dao.UserDao
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
+import com.onirutla.githubuser.data.source.local.entity.UserRemoteKey
 
-@Database(entities = [UserEntity::class], version = 1, exportSchema = false)
+@Database(entities = [UserEntity::class, UserRemoteKey::class], version = 1, exportSchema = false)
 abstract class GithubUserDatabase : RoomDatabase() {
-    abstract fun userDao(): UserDao
+    abstract val userDao: UserDao
+    abstract val userRemoteKeyDao: UserRemoteKeyDao
 }

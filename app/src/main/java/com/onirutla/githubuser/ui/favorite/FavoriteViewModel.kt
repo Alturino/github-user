1f12fa21878d3662478737ecd9307b46f07282bf
onirutlA <alturino001@gmail.com>
2022-09-24 12:37:18 +0700

    refactor: FavoriteViewModel.kt from stateflow to livedata

--- app/src/main/java/com/onirutla/githubuser/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/favorite/FavoriteViewModel.kt
@@ -1,14 +1,13 @@
 package com.onirutla.githubuser.ui.favorite
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.onirutla.githubuser.data.Resource
+import com.onirutla.githubuser.data.UIState
 import com.onirutla.githubuser.data.repository.UserRepository
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,10 +15,6 @@ class FavoriteViewModel @Inject constructor(
     userRepository: UserRepository
 ) : ViewModel() {
 
-    val favorites: StateFlow<Resource<List<UserEntity>>> =
-        userRepository.getFavorite().stateIn(
-            initialValue = Resource.Loading(),
-            scope = viewModelScope,
-            started = WhileSubscribed(5000)
-        )
+    val favorites: LiveData<UIState<List<UserEntity>>> = userRepository.getFavorite()
+        .asLiveData(viewModelScope.coroutineContext)
 }

224951fc0ddbbc91c723fa6ab026fc18799f1f14
onirutlA <alturino001@gmail.com>
2022-09-24 11:56:48 +0700

    refactor: change from stateflow to livedata

--- app/src/main/java/com/onirutla/githubuser/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/onirutla/githubuser/ui/detail/DetailViewModel.kt
@@ -1,38 +1,28 @@
 package com.onirutla.githubuser.ui.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
-import com.onirutla.githubuser.data.Resource
+import com.onirutla.githubuser.data.UIState
 import com.onirutla.githubuser.data.repository.UserRepository
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
-import com.onirutla.githubuser.util.MainDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@ExperimentalCoroutinesApi
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     private val userRepository: UserRepository,
-    @MainDispatcher private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
-    private val _username = MutableSharedFlow<String>()
+    private val _username = MutableLiveData<String>()
 
-    val user: StateFlow<Resource<UserEntity>> = _username.flatMapLatest {
-        userRepository.getDetailBy(it)
-    }.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(5000),
-        initialValue = Resource.Loading()
-    )
+    val user: LiveData<UIState<UserEntity>> = _username.switchMap {
+        userRepository.getDetailBy(it).asLiveData(viewModelScope.coroutineContext)
+    }
 
     fun setFavorite(userEntity: UserEntity) {
         viewModelScope.launch {
@@ -41,8 +31,6 @@ class DetailViewModel @Inject constructor(
     }
 
     fun getUser(username: String) {
-        viewModelScope.launch(dispatcher) {
-            _username.emit(username)
-        }
+        _username.value = username
     }
 }

eb598c798002c346578b2f5d1c33cf55cd3fde31
onirutlA <alturino001@gmail.com>
2022-10-03 16:06:44 +0700

    refactor: UserRepository.kt

--- app/src/main/java/com/onirutla/githubuser/data/repository/UserRepository.kt
+++ app/src/main/java/com/onirutla/githubuser/data/repository/UserRepository.kt
@@ -1,20 +1,20 @@
 package com.onirutla.githubuser.data.repository
 
 import androidx.paging.PagingData
-import com.onirutla.githubuser.data.Resource
+import com.onirutla.githubuser.data.UIState
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     fun searchByPaging(username: String): Flow<PagingData<UserEntity>>
 
-    fun getDetailBy(username: String): Flow<Resource<UserEntity>>
+    fun getDetailBy(username: String): Flow<UIState<UserEntity>>
 
     fun getFollowerPaging(username: String): Flow<PagingData<UserEntity>>
 
     fun getFollowingPaging(username: String): Flow<PagingData<UserEntity>>
 
-    fun getFavorite(): Flow<Resource<List<UserEntity>>>
+    fun getFavorite(): Flow<UIState<List<UserEntity>>>
 
     suspend fun setFavorite(userDto: UserEntity): UserEntity
 }

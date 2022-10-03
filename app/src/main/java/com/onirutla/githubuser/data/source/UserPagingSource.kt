a62153ea63624313c40db03af239fa05af0d0139
onirutlA <alturino001@gmail.com>
2022-10-03 16:05:33 +0700

    refactor: UserPagingSource.kt

--- app/src/main/java/com/onirutla/githubuser/data/source/UserPagingSource.kt
+++ app/src/main/java/com/onirutla/githubuser/data/source/UserPagingSource.kt
@@ -4,7 +4,7 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.onirutla.githubuser.data.source.local.entity.UserEntity
 import com.onirutla.githubuser.data.source.remote.response.UserResponse
-import com.onirutla.githubuser.data.source.remote.response.toEntity
+import com.onirutla.githubuser.util.toEntities
 import okio.IOException
 import retrofit2.HttpException
 import retrofit2.Response
@@ -22,7 +22,7 @@ class UserPagingSource(
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, UserEntity> = try {
         val position = params.key ?: 1
         val response = apiService(position).body()
-        val users = response?.map { it.toEntity() }
+        val users = response?.toEntities()
         val nextKey = if (users.isNullOrEmpty()) null else position + 1
         LoadResult.Page(
             data = users.orEmpty(),

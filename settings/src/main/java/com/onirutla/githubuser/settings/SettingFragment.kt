b53383378ee352db366fc5f93ee4f5aae87e52e9
onirutlA <alturino001@gmail.com>
2022-10-07 14:15:55 +0700

    modularize settings module

--- settings/src/main/java/com/onirutla/githubuser/settings/SettingFragment.kt
+++ settings/src/main/java/com/onirutla/githubuser/settings/SettingFragment.kt
@@ -1,4 +1,4 @@
-package com.onirutla.githubuser.ui.setting
+package com.onirutla.githubuser.settings
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,8 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.transition.MaterialFadeThrough
-import com.onirutla.githubuser.R
-import com.onirutla.githubuser.databinding.FragmentSettingBinding
+import com.onirutla.githubuser.settings.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
 

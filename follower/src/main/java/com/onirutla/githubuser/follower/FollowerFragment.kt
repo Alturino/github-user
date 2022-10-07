50bdce189b3e8ad10734236a5707f3d9573680b0
onirutlA <alturino001@gmail.com>
2022-10-07 09:27:41 +0700

    comment error in FollowerFragment.kt

--- follower/src/main/java/com/onirutla/githubuser/follower/FollowerFragment.kt
+++ follower/src/main/java/com/onirutla/githubuser/follower/FollowerFragment.kt
@@ -5,19 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.findNavController
-import com.onirutla.githubuser.core.adapter.UserPagingAdapter
 import com.onirutla.githubuser.follower.databinding.FragmentFollowerBinding
-import com.onirutla.githubuser.ui.SharedViewModel
-import com.onirutla.githubuser.ui.detail.DetailFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
@@ -27,14 +18,14 @@ class FollowerFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: FollowerViewModel by viewModels()
-    private val activityViewModel: SharedViewModel by activityViewModels()
+//    private val activityViewModel: SharedViewModel by activityViewModels()
 
-    private val followerAdapter by lazy {
-        UserPagingAdapter { view, user ->
-            view.findNavController()
-                .navigate(DetailFragmentDirections.actionDetailFragmentSelf(user.username))
-        }
-    }
+//    private val followerAdapter by lazy {
+//        UserPagingAdapter { view, user ->
+//            view.findNavController()
+//                .navigate(DetailFragmentDirections.actionDetailFragmentSelf(user.username))
+//        }
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,22 +38,22 @@ class FollowerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        activityViewModel.username.observe(viewLifecycleOwner) { username ->
-            viewModel.getUser(username)
-        }
+//        activityViewModel.username.observe(viewLifecycleOwner) { username ->
+//            viewModel.getUser(username)
+//        }
+//
+//        viewLifecycleOwner.lifecycleScope.launch {
+//            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                viewModel.user.collect {
+//                    followerAdapter.submitData(it)
+//                }
+//            }
+//        }
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.user.collect {
-                    followerAdapter.submitData(it)
-                }
-            }
-        }
-
-        binding.rvUser.apply {
-            adapter = followerAdapter
-            setHasFixedSize(true)
-        }
+//        binding.rvUser.apply {
+//            adapter = followerAdapter
+//            setHasFixedSize(true)
+//        }
     }
 
     override fun onDestroyView() {

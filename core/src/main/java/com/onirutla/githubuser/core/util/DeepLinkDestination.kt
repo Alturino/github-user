42dbacd2b3ca0de966d5801b719f34e62e598d24
onirutlA <alturino001@gmail.com>
2022-10-07 20:48:59 +0700

    add: Detail to DeepLinkDestination.kt

--- core/src/main/java/com/onirutla/githubuser/core/util/DeepLinkDestination.kt
+++ core/src/main/java/com/onirutla/githubuser/core/util/DeepLinkDestination.kt
@@ -4,6 +4,7 @@ import com.onirutla.githubuser.core.util.Constant.BASE_DEEP_LINK
 
 sealed class DeepLinkDestination(val route: String) {
     object Search : DeepLinkDestination("${BASE_DEEP_LINK}search")
+    object Detail : DeepLinkDestination("${BASE_DEEP_LINK}detail")
     object Follower : DeepLinkDestination("${BASE_DEEP_LINK}follower")
     object Following : DeepLinkDestination("${BASE_DEEP_LINK}following")
     object Favorite : DeepLinkDestination("${BASE_DEEP_LINK}favorite")

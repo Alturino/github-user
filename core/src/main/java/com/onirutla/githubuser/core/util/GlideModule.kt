b9c91ae3fafb425b8de4273dff5fde4e0fc4fb8d
onirutlA <alturino001@gmail.com>
2022-10-13 15:03:45 +0700

    refactor: remove GlideModule.kt

--- core/src/main/java/com/onirutla/githubuser/core/util/GlideModule.kt
+++
@@ -1,7 +0,0 @@
-package com.onirutla.githubuser.core.util
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class GlideModule : AppGlideModule()

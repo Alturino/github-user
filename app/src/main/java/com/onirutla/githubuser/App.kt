3b43a4d729f4df7b65e381d37c6f223c419a516e
onirutlA <alturino001@gmail.com>
2022-10-13 15:04:58 +0700

    add: App.kt implements of ImageLoaderFactory

--- app/src/main/java/com/onirutla/githubuser/App.kt
+++ app/src/main/java/com/onirutla/githubuser/App.kt
@@ -1,7 +1,17 @@
 package com.onirutla.githubuser
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(applicationContext)
+            .crossfade(true)
+            .allowHardware(false)
+            .build()
+    }
+}
